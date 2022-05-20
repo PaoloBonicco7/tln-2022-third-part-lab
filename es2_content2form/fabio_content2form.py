8e87a185892f3e96bd54447bc1a02065b2723fe5
Fabio Luciani <lu.fa.98@gmail.com>
2022-05-20 10:06:23 +0200

    added fabio's version for content2form

---
+++ es2_content2form/fabio_content2form.py
@@ -0,0 +1,38 @@
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet as wn
+from nltk.corpus import stopwords
+from collections import Counter
+from nltk.wsd import lesk
+
+def word_sense_disambiguation(list_words, word):
+    right_synset = lesk(list_words, word)
+    return right_synset
+
+def remove_stop_words(row):
+    stop_words = set(stopwords.words('english'))
+    punctuation = [',', '.', ';', '!', '?', "'", "''", '"', "’", "’’", "`","``"]
+    filtered_sentence = [w for w in row if not w.lower() in stop_words and w not in punctuation]
+    return filtered_sentence
+
+def calculate_frequency(row):
+    freq_dict = {}
+    row = remove_stop_words(word_tokenize(row))
+    for word in row[1:]:
+        if word.lower() not in freq_dict:
+            freq_dict[word.lower()] = 1
+        else:
+            freq_dict[word.lower()] += 1
+    return freq_dict
+
+
+with open ('/home/fazza/Documents/tln-2022-third-part-lab/def.csv', 'r') as f:
+    for row in f:
+        dict = calculate_frequency(row)
+        k = Counter(dict)
+        most_freq = k.most_common(3)
+        
+        for word in most_freq:
+            print(f"- Word: {word} \n{wn.synsets(word[0])}")
+        
+        print(f"Le parole più utilizzate sono: {most_freq}\n\n")
+        
